6c963a9303c6d9216c36d579984cdbc466f6f71f
ultra-000 <157633207+ultra-000@users.noreply.github.com>
2025-02-10 15:45:02 +0200

    additional functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+const printBtn = document.getElementById("printBtn");
+const pdfUrlInput = document.getElementById("pdfUrl");
+
 function printPdf(pdfUrl) {
   const newTab = window.open(pdfUrl, "_blank");
 
@@ -7,3 +10,12 @@ function printPdf(pdfUrl) {
     }, 500); // delay to ensure that the new tap has loaded
   }
 }
+
+printBtn.addEventListener("click", () => {
+  const pdfUrl = pdfUrlInput.value;
+  printPdf(pdfUrl);
+});
+
+window.onload = () => {
+  pdfUrlInput.value = window.location.href + "assets/pdf/sample.pdf";
+};
